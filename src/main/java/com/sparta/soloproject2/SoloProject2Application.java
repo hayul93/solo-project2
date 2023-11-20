0613f2bbc0faa32c3078abbff00cdc6e1f04146b
hayul93 <parksy785@gmail.com>
2023-11-20 10:43:42 +0900

    init commit

---
+++ src/main/java/com/sparta/soloproject2/SoloProject2Application.java
@@ -0,0 +1,13 @@
+package com.sparta.soloproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloProject2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoloProject2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/soloproject2/SoloProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.soloproject2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
