36807610e6c0460b35cfd5c279e3f4b05e9bf340
hayul93 <parksy785@gmail.com>
2023-11-22 16:39:26 +0900

    회원 가입 API 및 중복 Username 예외처리 구현

---
+++ src/main/java/com/sparta/soloproject2/UserService.java
@@ -0,0 +1,26 @@
+package com.sparta.soloproject2;
+
+import lombok.RequiredArgsConstructor;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+//    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+    public void signup(UserRequestDto userRequestDto) {
+        String username = userRequestDto.getUsername();
+        String password = userRequestDto.getPassword();//passwordEncoder.encode
+
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 유저 입니다.");
+        }
+
+        User user = new User(username, password);
+        userRepository.save(user);
+
+    }
+}
